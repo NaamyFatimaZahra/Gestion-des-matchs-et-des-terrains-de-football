da333da21bb00b436692be3de5d9146797b559a6
naamyfatimazahra <naamyfatimazahra@gmail.com>
2025-04-04 16:08:44 +0000

    cree migration et model des users et les role

---
+++ app/Models/Role.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+   
+     public $timestamps = false;
+     protected $fillable = [
+        'id',
+        'name',
+        
+    ];
+
+
+     public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ database/migrations/0001_01_000000_create_role_table.php
@@ -0,0 +1,29 @@
+<!-- 0001_01_000000_create_role_table.php -->
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->enum('name',['admin','Propriétaire','Joueur']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
